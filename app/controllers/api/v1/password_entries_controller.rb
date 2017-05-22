5b51ad198dd0b0369519584ab4e358fee125781b
Magdalena Waligórska <magda@prograils.com>
2017-05-22 18:58:31 +0200

    the rest of api methods added

--- app/controllers/api/v1/password_entries_controller.rb
+++ app/controllers/api/v1/password_entries_controller.rb
@@ -1,7 +1,34 @@
 class Api::V1::PasswordEntriesController < Api::V1::BaseController
+  before_action :find_password_entry, except: [:create]
+
   def show
-    entry = current_account.password_entries.find(params[:id])
-    entry.master_password = request.headers['Password']
-    respond_with entry
+    @entry.master_password = request.headers['Password']
+    respond_with @entry
+  end
+
+  def create
+    @entry = current_account.password_entries.create(entry_params)
+    respond_with :api, :v1, @entry
+  end
+
+  def update
+    @entry.update(entry_params)
+    respond_with @entry
+  end
+
+  def destroy
+    respond_with @entry.destroy
+  end
+
+  private
+
+  def find_password_entry
+    @entry = current_account.password_entries.find(params[:id])
+  end
+
+  def entry_params
+    p = params.require(:password_entry).permit(:site_name, :site_url,
+                                               :username, :raw_password)
+    p.merge(master_password: request.headers['Password'])
   end
 end
--- test/controllers/api/v1/password_entries_controller_test.rb
+++ test/controllers/api/v1/password_entries_controller_test.rb
@@ -4,19 +4,54 @@ class Api::V1::PasswordEntriesControllerTest < ActionController::TestCase
   setup do
     @account = FactoryGirl.create :account
     @password_entry = FactoryGirl.create :password_entry, account: @account
-   end
+    @password_entry_params = FactoryGirl.attributes_for(:password_entry)
+    request.headers['Username'] = @account.username
+    request.headers['Password'] = 'MasterPassword'
+  end
 
   test 'must not show password_entry if not providing auth data' do
+    request.headers['Password'] = 'AnotherPassword'
     get :show, params: { id: @password_entry.id }, format: :json
     assert_response 401
     assert_equal response.body, { errors: 'Not authenticated' }.to_json
   end
 
   test 'must show password_entry if auth data provided' do
-    request.headers['Username'] = @account.username
-    request.headers['Password'] = 'MasterPassword'
     get :show, params: { id: @password_entry.id }, format: :json
     assert_response :success
     assert_equal response.body, @password_entry.to_json
+  end
+
+  test 'must create password_entry if auth data provided' do
+    assert_difference 'PasswordEntry.count', 1 do
+      post :create,
+           params: { password_entry: @password_entry_params },
+           format: :json
+    end
+    assert_response :success
+    assert_equal response.body, @password_entry.to_json
+  end
+
+  test 'must update password_entry if auth data provided' do
+    @password_entry_params[:site_name] = 'NewSiteName'
+    put :update,
+        params: { id: @password_entry.id,
+                  password_entry: @password_entry_params },
+        format: :json
+    assert_response :success
+    assert_empty response.body
+
+    @password_entry.reload
+    assert_equal @password_entry.site_name, 'NewSiteName'
+  end
+
+  test 'must destroy password_entry if auth data provided' do
+    assert_difference 'PasswordEntry.count', -1 do
+      delete :destroy,
+          params: { id: @password_entry.id },
+          format: :json
+    end
+    assert_response :success
+    assert_empty response.body
   end
 end
